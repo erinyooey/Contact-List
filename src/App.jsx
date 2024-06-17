7297e4943580be64be000d5dbe837e3c2010a83e
Erin Yoo <yooeh@masters.edu>
2024-06-17 13:17:43 -0700

    setting up the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
+import ContactList from './components/ContactList'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <ContactList />
     </>
   )
 }
