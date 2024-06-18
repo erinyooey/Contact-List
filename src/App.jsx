d82508f716444d503986ced1be36d2781daaa79d
Erin Yoo <yooeh@masters.edu>
2024-06-18 15:36:50 -0700

    Modified and finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import './App.css'
-import axios from 'axios'
 import ContactList from './components/ContactList'
+import SelectedContact from './components/SelectedContact'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedContactId, setSelectedContactId] = useState(null)
 
   return (
     <>
-      <ContactList />
+      {
+        selectedContactId ? (<SelectedContact selectedContactId = {selectedContactId} setSelectedContactId = {setSelectedContactId} />) : (<ContactList setSelectedContactId = {setSelectedContactId} />)
+      }
     </>
   )
 }
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,29 +1,49 @@
 import React, { useState } from 'react'
 import ContactRow from './ContactRow';
+import { useEffect } from 'react';
+import axios from 'axios';
+
+
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
   { id: 2, name: "C-3PO", phone: "333-333-3333", email: "c3po@droids.com" },
   { id: 3, name: "BB-8", phone: "888-888-8888", email: "bb8@droids.com" },
 ];
-export default function ContactList() {
+export default function ContactList({setSelectedContactId}) {
   const [contacts, setContacts] = useState(dummyContacts)
   console.log("Contacts: ", contacts)
+
+  useEffect(()=>{
+    fetchContacts();
+  }, [])
+
+  const fetchContacts = async()=>{
+    try {
+      const response = await axios.get(`https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users`)
+      console.log(response.data)
+
+      setContacts(response.data) 
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <table>
         <thead>
-            <tr>
+            <tr className='head'>
                 <th colSpan="3">Contact List</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td>Name</td>
-                <td>Email</td>
-                <td>Phone</td>
+                <td><strong>Name</strong></td>
+                <td><strong>Email</strong></td>
+                <td><strong>Phone</strong></td>
             </tr>
-        {
+        { contacts &&
           contacts.map((contact)=>{
-            return <ContactRow key={contact.id} contact={contact}/>
+            return <ContactRow key={contact.id} contact={contact} setSelectedContactId={setSelectedContactId}/>
           })
 
         }
